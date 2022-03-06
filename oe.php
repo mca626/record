da205f68edc4f2847ea819abd3d8f391e306c286
Mariya Johnson <99708227+mca626@users.noreply.github.com>
2022-03-07 00:29:13 +0530

    Add files via upload

--- oe.php
+++ oe.php
@@ -1,21 +1,12 @@
-<html>  
-<body>  
-<form method="post">  
-    Enter a number:  
-    <input type="number" name="number">  
-    <input type="submit" value="Submit">  
-</form>  
-</body>  
-</html>  
-<?php   
-    if($_POST){  
-        $number = $_POST['number'];   
-        //divide entered number by 2   
-        //if the reminder is 0 then the number is even otherwise the number is odd  
-        if(($number % 2) == 0){  
-            echo "$number is an Even number";  
-        }else{  
-            echo "$number is Odd number";  
+  <?php     
+        $num= $_GET['num'];    
+        if(($num % 2) == 0)
+        {  
+    echo "$num is an Even number";  
+        }
+else
+{  
+   echo "$num is Odd number";  
         }  
-    }  
+   
 ?>  
